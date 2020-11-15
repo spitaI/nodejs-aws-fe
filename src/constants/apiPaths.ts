0307189c4d5057d18299927ea371ed2c897e98d0
Aliaksandr Martynau <alex.asm.1997@gmail.com>
2020-11-15 18:29:07 +0300

    feat: integrate import-service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://mb8luvjk4i.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://n5cx87r831.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://mb8luvjk4i.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
