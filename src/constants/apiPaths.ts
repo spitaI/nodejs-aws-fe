3add00f259b154880a61e1a2529d34863ac78fee
Aliaksandr Martynau <alex.asm.1997@gmail.com>
2020-12-06 17:58:34 +0300

    feat: integrate cart-api service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://n5cx87r831.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://mb8luvjk4i.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://ziwgp32rgd.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
